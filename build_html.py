e2fff010ea77b05f1a2a4eacf548b6efb8cb307b
Justin Ong <9336475+JustinOng@users.noreply.github.com>
2023-07-30 18:31:23 +0800

    Put back newline to prevent single line comments from causing issues

--- build_html.py
+++ build_html.py
@@ -3,11 +3,13 @@ with open("src/index.html") as f:
 
 with open("src/index_html.h", "w") as f:
     page = page.replace('"', '\\"')
-    page = "\n".join([f'"{line}"' for line in page.splitlines()])
-    f.write(f'''#ifndef INDEX_HTML_H
+    page = "\n".join([f'"{line}\\n"' for line in page.splitlines()])
+    f.write(
+        f"""#ifndef INDEX_HTML_H
 #define INDEX_HTML_H
 
 const char index_html[] = {page};
 
 #endif
-''')
+"""
+    )
